7c11525569a74288a7ffa949ae2d1533992a856b
unknown <jinbin@D2.HYRON.COM>
2020-08-20 10:02:09 +0900

    添加调试代码

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <arduino.h>
 #include <dev_74hc595.h>
 
-static const uint16_t sda_pin = 10;
-static const uint16_t sck_pin = 11;
-static const uint16_t push_pin = 12;
-static const uint16_t en_pin = 13;
+static const uint16_t sda_pin = 0;
+static const uint16_t sck_pin = 2;
+static const uint16_t push_pin = 5;
+static const uint16_t en_pin = 12;
 
 typedef struct {
     uint8_t seg;
@@ -12,17 +12,22 @@ typedef struct {
 } NixieTubeCtrl;
 
 void setup() {
+    Serial.begin(115200);
     // 定义一个以uint16_t为数据单位的595对象
     Dev74HC595<uint16_t> hc595(sda_pin, sck_pin, push_pin, en_pin);
+
+    Serial.println("设备初始化完毕");
     // 定义一个用于发送的数组
     uint16_t data[2] = {0xff00, 0xf0f0};
     // 发送整个数组到74hc595
+    Serial.println("开始发送数组到595");
     hc595.send(data, 2);
     uint16_t a = 0x2345;
-    // 发送1个数据到74hc595
+    // // 发送1个数据到74hc595
+    Serial.println("发送单个整形16位数据到595");
     hc595.send(&a);
     uint16_t b = 0x0045;
-    hc595 = a + b;
+    hc595 = b + a;
     hc595 << a << b;
 
     // Dev74HC595<NixieTubeCtrl> hc595_1(sda_pin, sck_pin, push_pin, en_pin);
